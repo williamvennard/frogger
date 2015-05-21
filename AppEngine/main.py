01d7793b7103a07385ad56414522adc3700aeab9
Charlie Slater <charlieslater@gmail.com>
2015-05-20 23:03:22 -0700

    Removed blank lines.

--- AppEngine/main.py
+++ AppEngine/main.py
@@ -2,11 +2,7 @@
 This is the "data importer" module.  
 
 It takes a http from input and stores it in the database.  It also allows you to display the results of the entry.
-
 """
-
-
-
 import os
 import webapp2
 import jinja2
@@ -16,13 +12,7 @@ import re
 from time import gmtime, strftime
 import collections
 import hashlib
-
-
 from google.appengine.ext import db
-
-
-
-
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = False)
@@ -30,7 +20,6 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 def render_str(template, **params):
 	t = jinja_env.get_template(template)
 	return t.render(params)
-
 
 class InstrumentDataHandler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
@@ -42,57 +31,43 @@ class InstrumentDataHandler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
-
 #def render_post(response, post):
 #	response.out.write('<b>' + input.description + '</b><br>')
 #	response.out.write(input.inputdata)
 
-
 def input_key(name = 'default'):
 	return db.Key.from_path('inputs', name)
-
 
 class Input(db.Model):
 	description = db.StringProperty(required = True)
 	inputdata= db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
-
 	def render(self):
 		self._render_text = self.inputdata.replace('\n', '<br>')
 		return render_str("post.html", p = self)
 
 class LinkPage(InstrumentDataHandler):
 	def get(self):
-		
 		datasets = db.GqlQuery("select * from Input order by created desc limit 10")
 		jsondata = datasets.get()
 		print "you are in the linkpage handler"
 		#datasets = jsondata.inputdata
 		description = jsondata.description
-		
 		self.render('data.html', datasets = datasets)
-
 
 class DataPage(InstrumentDataHandler):
 	def get(self):
-		
 		datasets = db.GqlQuery("select * from Input order by created desc limit 10")
 		jsondata = datasets.get()
 		#print "you are in the datapage handler"
 		#datasets = jsondata.inputdata
 		description = jsondata.description
-		
 		self.render('data.html', datasets = datasets)
 
-
-
 class InputPage(InstrumentDataHandler):
-
-
     def get(self):
         self.render("front.html")
         print "you are in the get handler"
-
     def post(self):
 		print "you are in the posthandler"
 		inputdata = self.request.get("inputdata")
@@ -104,7 +79,6 @@ class InputPage(InstrumentDataHandler):
 		#self.render("front.html")
 		self.redirect('/data')
 		
-
 PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
 app = webapp2.WSGIApplication([
 	('/input', InputPage),
