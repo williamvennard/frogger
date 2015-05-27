8b3d21f90997289fc703f2f075c8f989781ae254
Nick Edwards <nedwards@gradientone.com>
2015-05-27 14:45:09 -0700

    Changed the client ID credentials.

--- AppEngine/clientsimulation/main.py
+++ AppEngine/clientsimulation/main.py
@@ -9,8 +9,8 @@ class MainHandler(webapp2.RequestHandler):
 #                           headers={
 #                               'content-type':'application/x-www-form-urlencoded'},
 #                           data={
-#                   		    'client_id':'1023941599436-s58bk24id29kjbkpa1abn4l82ub2n3bk.apps.googleusercontent.com',
-#                               'client_secret':'2Wy0xFTfdvaJ6pg2xqaarNmS',
+#                   		    'client_id':'1023941599436-v1mlfpv9ukdv8lfnhkp4snp10f6rc537.apps.googleusercontent.com',
+#                               'client_secret':'wiLNYGbRIMRKOq_v_OM0yjN3',
 #                               'redirect_uri':'http://localhost/etc'})
 
 
