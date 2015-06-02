69ab709e027102eb1439b4024b177e7be65440b3
Charlie Slater <charlieslater@gmail.com>
2015-06-01 22:51:27 -0700

    make sure data are sorted.

--- Python/Instruments/csvpost.py
+++ Python/Instruments/csvpost.py
@@ -6,14 +6,17 @@ import time
 f = open('../../DataFiles/tekcsv/tek0012ALL.csv')
 f = itertools.islice(f, 18, 100)
 reader = csv.DictReader(f, fieldnames = ("TIME", "CH1", "CH2", "CH3"    , "CH4"))
-window = {'config':{'TEK':'TODO'},'slicename':'18to100','data':[row for row in reader]}
+def getKey(row):
+    return float(row['TIME'])
+
+datarows = [row for row in reader]
+datarows = sorted(datarows,key=getKey)
+window = {'config':{'TEK':'TODO'},'slicename':'18to100','data':datarows}
 out = json.dumps(window)
 #print "out =",out
-#url = "https://fiberboardfreeway.appspot.com/testnuc"
+#url = "https://fiberboardfreeway.appspot.com/oscopedata/tahoe-scope-2015-05-31-1500"
 url = "http://localhost:8080/oscopedata/tahoe-scope-2015-05-31-1500"
-#data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 r = requests.post(url, data=out, headers=headers)
-#print "dir(r)=",dir(r)
 print "r.reason=",r.reason
 print "r.status_code=",r.status_code
