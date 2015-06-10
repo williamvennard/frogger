0d7adcbdd0b6a4bfdf5e1de58d417d86ac0cc587
Nick Edwards <nedwards@gradientone.com>
2015-06-10 14:24:40 -0700

    removed tab

--- Python/Instruments/slicedcsvpost.py
+++ Python/Instruments/slicedcsvpost.py
@@ -12,7 +12,7 @@ from itertools import izip_longest
 from itertools import izip
 
 f = open('../../DataFiles/tekcsv/tek0012ALL.csv')
-f = itertools.islice(f, 18, 125)
+f = itertools.islice(f, 18, 100)
 sample_chunk = 10
 
 def grouper(iterable, n, fillvalue=None):
@@ -24,24 +24,24 @@ values = list(out)
 keys = ['TIME', 'CH1', 'CH2', 'CH3', 'CH4']
 
 for value in values:
-	new = {}
-	for item in value:
-		if item != 0:
-			item = item.strip()
-			k = item.split(",")
-		time = k[0]
-		new[time] = dict(zip(keys,k))
-	times = list(new.keys())
-	times = sorted(times, reverse=True)
-	slicename = str("slice%sto%s" % (float(times[0]), float(times[-1])))
-	window = {'config':{'TEK':'TODO'},'slicename':slicename,'data':new}
-	out = json.dumps(window)
-	url = "http://localhost:18080/oscopedata/amplifier/%s" % slicename
-	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-	r = requests.post(url, data=out, headers=headers)
-	print "dir(r)=",dir(r)
-	print "r.reason=",r.reason
-	print "r.status_code=",r.status_code
+    new = {}
+    for item in value:
+        if item != 0:
+            item = item.strip()
+            k = item.split(",")
+        time = k[0]
+        new[time] = dict(zip(keys,k))
+    times = list(new.keys())
+    times = sorted(times, reverse=True)
+    slicename = str("slice%sto%s" % (float(times[0]), float(times[-1])))
+    window = {'config':{'TEK':'TODO'},'slicename':slicename,'data':new}
+    out = json.dumps(window)
+    url = "http://localhost:18080/oscopedata/amplifier/%s" % slicename
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    r = requests.post(url, data=out, headers=headers)
+    print "dir(r)=",dir(r)
+    print "r.reason=",r.reason
+    print "r.status_code=",r.status_code
 
 
 
