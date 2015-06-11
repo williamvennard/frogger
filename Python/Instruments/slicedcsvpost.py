fc7556c9ec6a0d606ed9e16b8b87ef0bf6047bb9
Nick Edwards <nedwards@gradientone.com>
2015-06-11 10:40:41 -0700

    Created two functions to help with modularity.

--- Python/Instruments/slicedcsvpost.py
+++ Python/Instruments/slicedcsvpost.py
@@ -20,9 +20,28 @@ def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return izip_longest(*args, fillvalue=fillvalue)
 
+def slicename_creation(times):
+    modified_time = []
+    for time in times:
+        modified_time.append(float(time))
+    modified_time = sorted(modified_time, reverse=True)
+    slicename = "slice%sto%s" % (modified_time[0], modified_time[-1])
+    return slicename
+
+def post_creation(window, slicename):
+    out = json.dumps(window)
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    #url = "https://gradientone-test.appspot.com/oscopedata/amplifier/%s" % slicename
+    url = "http://localhost:18080/oscopedata/amplifier/%s" % slicename
+    r = requests.post(url, data=out, headers=headers)
+    print "dir(r)=",dir(r)
+    print "r.reason=",r.reason
+    print "r.status_code=",r.status_code
+
 out = grouper(f, sample_chunk, 0)
 values = list(out)
 keys = ['TIME', 'CH1', 'CH2', 'CH3', 'CH4']
+
 for value in values:
     new = {}
     for item in value:
@@ -32,20 +51,10 @@ for value in values:
         time = k[0]
         new[time] = dict(zip(keys,k))
     times = list(new.keys())
-    modified_time = []
-    for time in times:
-        modified_time.append(float(time))
-    modified_time = sorted(modified_time, reverse=True)
-    slicename = "slice%sto%s" % (modified_time[0], modified_time[-1])
+    slicename =  slicename_creation(times)
     window = {'config':{'TEK':'TODO'},'slicename':slicename,'data':new}
-    out = json.dumps(window)
-    url = "https://gradientone-test.appspot.com/oscopedata/amplifier/%s" % slicename
-    url = "http://localhost:18080/oscopedata/amplifier/%s" % slicename
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    r = requests.post(url, data=out, headers=headers)
-    print "dir(r)=",dir(r)
-    print "r.reason=",r.reason
-    print "r.status_code=",r.status_code
+    post_creation(window, slicename)
+
 
 #https://gradientone-test.appspot.com/oscopedata/default-scope
 
