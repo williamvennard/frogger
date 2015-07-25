ed1df0f5dc6b860a611642d885ce52849b3b086a
Nick Edwards <nedwards@gradientone.com>
2015-07-25 12:52:18 -0700

    update

--- AppEngine/testconfiginput.py
+++ AppEngine/testconfiginput.py
@@ -5,6 +5,8 @@ from onedb import ConfigDB
 from onedb import ConfigDB_key
 from onedb import TestDB
 from onedb import TestDB_key
+from onedb import DutDB
+from onedb import DutDB_key
 import jinja2
 import json
 import logging
@@ -39,8 +41,13 @@ class Handler(InstrumentDataHandler):
         RMS_time_stop = (self.request.get('RMS_time_stop'))
         sample_rate = (self.request.get('sample_rate'))
         number_of_samples = (self.request.get('number_of_samples'))
+        dut_name = self.request.get('dut_name')
+        dut_type = self.request.get('dut_type')
+        settings = self.request.get('settings')
         testpost = (self.request.get('testpost'))
         instpost = (self.request.get('instpost'))
+        dutpost = (self.request.get('dutpost'))
+
         if testpost == 'True': #this controls the POST functionality if someone is configuring test plan details.
             print 'hi'
             t = TestDB(parent = TestDB_key(testplan_name), 
@@ -76,5 +83,28 @@ class Handler(InstrumentDataHandler):
             if insts.key() not in test.instruments:  #add the instrument name to the list property ot the test plan
                 test.instruments.append(insts.key())
                 test.put()
+        if dutpost == 'True': #this controls the POST functionality if someone is configuring instrument details.
+            tests = TestDB.gql("Where testplan_name =:1", testplan_name).get()
+            dut = DutDB.gql("Where dut_name =:1", dut_name).get()
+            if dut == None:  #if there is not an instrument with the inputted name, then create it in the DB
+                d = DutDB(parent = DutDB_key(dut_name), 
+                    company_nickname = company_nickname, author = author,
+                    dut_type = dut_type,
+                    dut_name = dut_name,             
+                    settings = settings,
+                    )
+                d.put()
+            dut = DutDB.gql("Where dut_name =:1", dut_name).get()
+            if dut == None:
+                print 'no dut found the first time'
+                dut = DutDB.gql("Where dut_name =:1", dut_name).get()
+            if tests.key() not in dut.tests:  #add the test plan to the list property of the dut
+                dut.tests.append(tests.key())
+                dut.put()
+            duts = DutDB.gql("Where dut_name =:1", dut_name).get()
+            test = TestDB.gql("Where testplan_name =:1", testplan_name).get()
+            if duts.key() not in test.duts:  #add the  dut name to the list property ot the test plan
+                test.duts.append(duts.key())
+                test.put()    
         self.render('testconfig.html')
         #self.redirect('/testplansummary/' + company_nickname + '/' + hardware_name)
