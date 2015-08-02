7827b812404d64471a86de68ed628cb8e5f30792
Nick Edwards <nickedwards@gmail.com>
2015-08-02 11:30:21 -0700

    Updated

--- Python/Instruments/exp_bscope_client.py
+++ Python/Instruments/exp_bscope_client.py
@@ -177,8 +177,12 @@ def bscope_acq_exp(config, s):
             #print "Finished: Library closed, resources released."    
             bits = BitScope(acq_dict, s)
             bits.transmitdec()
-            #r = s.get(config_url)
-            break
+            r = s.get(config_url)
+            config = r.json()
+            if config['configs']:
+                config = config['configs'][0]
+                if config['commence_explore'] == 'False':
+                    break
             time_dec = time.time()
             #bits.transmitraw()
             time_stop = time.time()
@@ -193,11 +197,90 @@ def bscope_acq_exp(config, s):
             #print 'time after dec transmission and transmit raw data', raw_time
             print 'total time', total_time
         print "do bl close"
-        do test complete
+       # do test complete
         #time.sleep(5)
     else:
         print "  FAILED: device not found (check your probe file)."
 
+def bscope_acq(config, s):    
+    """sets the configuration for the bitscope API and calls the BitScope class"""
+    time_start = time.time()
+    acq_dict = {}
+    print "Starting: Attempting to open one device..."
+    if BL_Open(MY_PROBE_FILE,1):
+        post_status('Acquiring')
+        config_vars = check_config_vars(config)
+        active_testplan_name = config_vars[0]
+        config_name = config_vars[1]
+        MY_RATE = float(config_vars[2])
+        MY_SIZE = int(config_vars[3])
+        test_plan = config_vars[4]
+        BL_Select(BL_SELECT_DEVICE,MY_DEVICE)
+        BL_Mode(BL_MODE_LOGIC) == BL_MODE_LOGIC or BL_Mode(BL_MODE_FAST)
+        BL_Range(BL_Count(BL_COUNT_RANGE))
+        BL_Mode(MY_MODE) # prefered capture mode
+        BL_Intro(BL_ZERO); # optional, default BL_ZERO
+        BL_Delay(BL_ZERO); # optional, default BL_ZERO
+        BL_Rate(MY_RATE); # optional, default BL_MAX_RATE
+        BL_Size(MY_SIZE); # optional default BL_MAX_SIZE
+        BL_Select(BL_SELECT_CHANNEL,MY_CHANNEL); # choose the channel
+        BL_Trigger(BL_ZERO,BL_TRIG_RISE); # optional when untriggered */
+        BL_Select(BL_SELECT_SOURCE,BL_SOURCE_POD); # use the POD input */
+        BL_Range(BL_Count(BL_COUNT_RANGE)); # maximum range
+        BL_Offset(BL_ZERO); # optional, default 0
+        BL_Enable(TRUE); # at least one channel must be initialised 
+        BL_Trace()
+        tse = dt2ms(datetime.datetime.now())
+        DATA = BL_Acquire()
+        SAMPLE_SIZE = len(DATA)
+        MY_SAMPLE_INTERVAL = int(1/MY_RATE*1000000) #interval between sample in microsec
+        print MY_SAMPLE_INTERVAL, 'sample interval'
+        Total_Slices = math.ceil((float(SAMPLE_SIZE)/(float(SLICE_SIZE))/float(MY_RATE/1000.0)))
+        print 'total slices', Total_Slices
+        config_dict = {}
+        plot_dict = {}
+        inst_dict ={}
+        inst_dict = set_v_for_k(inst_dict, 'Link', BL_Name(0))
+        inst_dict = set_v_for_k(inst_dict, 'BitScope', [BL_Version(BL_VERSION_DEVICE), BL_ID()])
+        inst_dict = set_v_for_k(inst_dict, 'Channels', [BL_Count(BL_COUNT_ANALOG) + BL_Count(BL_COUNT_LOGIC), 
+                                                           BL_Count(BL_COUNT_ANALOG),BL_Count(BL_COUNT_LOGIC)])
+        inst_dict = set_v_for_k(inst_dict, 'Library', [BL_Version(BL_VERSION_LIBRARY), BL_Version(BL_VERSION_BINDING)]) 
+        inst_dict = set_v_for_k(inst_dict, 'Sample_Rate_Hz', MY_RATE)
+        inst_dict = set_v_for_k(inst_dict, 'Sample_Size', SAMPLE_SIZE)
+        plot_dict = set_v_for_k(plot_dict, 'Total_Slices', Total_Slices)
+        plot_dict = set_v_for_k(plot_dict, 'Slice_Size_msec', SLICE_SIZE)
+        plot_dict = set_v_for_k(plot_dict, 'Raw_msec_btw_samples', MY_SAMPLE_INTERVAL)
+        plot_dict = set_v_for_k(plot_dict, 'Start_TSE', str(roundup(tse)))
+        config_dict = set_v_for_k(config_dict,'Plot_Settings', plot_dict)
+        config_dict = set_v_for_k(config_dict,'Inst_Settings', inst_dict)
+        acq_dict = set_v_for_k(acq_dict, 'data', DATA) 
+        acq_dict = set_v_for_k(acq_dict, 'i_settings', inst_dict)    
+        acq_dict = set_v_for_k(acq_dict, 'p_settings', plot_dict)
+        acq_dict = set_v_for_k(acq_dict, 'active_testplan_name', active_testplan_name)
+        acq_dict = set_v_for_k(acq_dict, 'config_name', config_name)
+        acq_dict = set_v_for_k(acq_dict, 'test_plan', test_plan)
+        acq_dict = set_v_for_k(acq_dict, 'Start_TSE', (roundup(tse)))
+        BL_Close()
+        time_bs = time.time()
+        print "Finished: Library closed, resources released."    
+        bits = BitScope(acq_dict,s)
+        bits.transmitdec()
+        time_dec = time.time()
+        bits.transmitraw()
+        time_stop = time.time()
+        #bits.transmitblob()
+        bits.testcomplete()
+        post_status('Idle')
+        bs_time = time_bs - time_start
+        dec_time = time_dec - time_start
+        raw_time = time_stop - time_dec
+        total_time = time_stop - time_start
+        print 'BS data acq time', bs_time
+        #print 'time between data acq and transmit dec data', dec_time
+        #print 'time after dec transmission and transmit raw data', raw_time
+        print 'total time', total_time
+    else:
+        print "  FAILED: device not found (check your probe file)."
 
 #post_status('Idle')
 check_config_url()
