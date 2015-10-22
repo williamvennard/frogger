36dce147d2e71f1931e98a3ad1c2acb80cf0a439
Nick Edwards <nedwards@gradientone.com>
2015-10-22 14:55:12 -0700

    fixed

--- Python/Instruments/test_client.py
+++ Python/Instruments/test_client.py
@@ -12,8 +12,9 @@ import scipy.signal
 import ivi
 
 pm = ivi.agilent.agilentU2001A("USB::0x0957::0x2b18::INSTR")
-print 'old pm', pm.initialized
+print 'old pm inititalized should be true', pm.initialized
 pm.close()
+print 'old pm not initialized should be false', pm.initialized
 new_pm = ivi.agilent.agilentU2001A("USB::0x0957::0x2b18::INSTR")
 print 'new pm', new_pm.initialized
 new_m.close()
