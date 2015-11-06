ea17a8fa3bdc236e0b5b17589000be37d741eef6
williamvennard <wvennard@gradientone.com>
2015-11-06 15:47:08 -0700

    fixed css layout of buttons and config form for power meter

--- AppEngine/static/js/powerMeter.js
+++ AppEngine/static/js/powerMeter.js
@@ -1,7 +1,7 @@
 function powerMeterStart() {
   console.log('PowerMeter START!');
-      //test_info_url = 'https://gradientone-test.appspot.com/u2000_testresults/Acme/batch1/Production';
-      test_info_url = window.location.origin + '/u2000_testresults/Acme/batch1/Production';
+      //test_info_url = 'https://gradientone-test.appspot.com/u2000_traceresults/Acme/MSP/Production';
+      test_info_url = window.location.origin + '/u2000_traceresults/Acme/MSP/Production';
       $.ajax({
           async: true,
           url: test_info_url,
@@ -16,23 +16,55 @@ function powerMeterStart() {
         //def get(self,company_nickname="", hardware_name="",config_name="",start_tse=""):
         //u2000data
         
-        //DECIMATED DATA
-        //var decData = testInfo.window_bscope.cha;
-        //console.log('getTestInfo: decData =',decData);
-        //drawDecChart(decData);
-
-        //testSettings = testInfo.p_settings;
-        //testSliceStart = testSettings.Start_TSE;    
-        //decPointSpacing = (Number(testSettings.Dec_msec_btw_samples))/1000000;
-        //decPointSpacing = (0.002)   
-        //console.log('exploreMode: decPointSpacing = ', decPointSpacing);
-        //instrumentName = 'Instrument: ' + testInfo.config_name;
-        //hardwareName = 'Hardware: ' + testInfo.hardware_name;
-        //document.getElementById("instrumentName").innerHTML = instrumentName;
         document.getElementById("measurementValue").innerHTML = measurement;
        });
     //exploreTimerID = setTimeout(exploreMode,50);
 };
+
+//CONFIG FORM SUBMIT
+    function PMConfig() {
+      $('#collapseConfig').collapse("hide");
+      var d = document.getElementById('configSettings');
+        var children = d.childNodes;
+/*
+      var configName = children[1].children[1].value;
+      var traceName = children[3].children[1].value;
+      //var configBand = children[5].children[1].value;
+      var configSampleRate =  children[5].children[1].value;
+      var configSampleSize = children[7].children[1].value; 
+      //var configResolution = children[11].children[1].value; 
+      //var configChNum = children[11].children[1].value; 
+
+      var configSettings = document.getElementById('settingsDisplay');
+        var setChildren = configSettings.childNodes;
+
+
+
+      setChildren[1].children[1].innerHTML = configName;
+      setChildren[3].children[1].innerHTML = configSampleSize;
+      setChildren[1].children[3].innerHTML = traceName;
+      setChildren[3].children[3].innerHTML = configSampleRate;
+*/
+      var config_url = 'https://gradientone-test.appspot.com/bscopeconfiginput';
+      console.log('saveStatus: config_url = ',config_url);
+
+      var configSettings = JSON.stringify({"config_name":configName,"analog_sample_rate":configSampleRate,
+       "capture_buffer_size":configSampleSize,"trace_name":traceName, "hardware_name":"Tahoe",
+       "inst_name":"BitScope","company_nickname":"Acme"});
+      console.log('instConfig: configSettings = ',configSettings);
+     $.ajax({
+        type: "POST",
+        url: config_url,
+        data: configSettings,
+        dataType: 'json',
+        success: function(data, textStatus, jqXHR)
+        {
+            console.log('saveStatus: Ajax post was a success!');
+        },
+      }); 
+
+
+    };
 
 $("#powerMeterStartStop").click(function() {
       $(this).text(function(i, v){
