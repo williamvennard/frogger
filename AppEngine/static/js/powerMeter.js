52bf8922190aee61012b5557b80025e3ee577db5
williamvennard <wvennard@gradientone.com>
2015-11-06 18:14:03 -0700

    unhardcoded config_name

--- AppEngine/static/js/powerMeter.js
+++ AppEngine/static/js/powerMeter.js
@@ -1,9 +1,66 @@
+//PM CONFIG FORM SUBMIT
+var configName;
+function PMConfig() {
+      $('#collapseConfig').collapse("hide");
+      var d = document.getElementById('PMConfigSettings');
+
+      var children = d.childNodes;
+      console.log('PMConfig: children = ', children);
+      configName = children[1].children[1].value;
+      var traceName = children[3].children[1].value;
+
+      var frequencyCorrection =  children[5].children[1].value;
+      var offset = children[7].children[1].value; 
+      var units = children[9].children[1].value; 
+      var avgCountAuto = children[11].children[1].checked; 
+      var rangeAuto = children[13].children[1].checked;
+
+      var configSettings = document.getElementById('PMSettingsDisplay');
+      var setChildren = configSettings.childNodes;
+
+
+
+      setChildren[1].children[1].innerHTML = configName;
+      setChildren[3].children[1].innerHTML = frequencyCorrection;
+      setChildren[1].children[3].innerHTML = traceName;
+      setChildren[3].children[3].innerHTML = offset;
+
+      document.getElementById("PMUnits").innerHTML = units;
+    
+
+      var config_url = window.location.origin + '/u2000_configinput';
+      console.log('saveStatus: config_url = ',config_url);
+
+      var configSettings = JSON.stringify({"config_name":configName,"trace_name":traceName, "correction_frequency":frequencyCorrection,
+       "offset":offset, "units":units, "avg_count_auto":avgCountAuto, "range_auto":rangeAuto, "hardware_name":"Tahoe",
+       "inst_name":"U2001A","company_nickname":"Acme"});
+
+      console.log('instConfig: configSettings = ',configSettings);
+
+     $.ajax({
+        type: "POST",
+        url: config_url,
+        data: configSettings,
+        dataType: 'json',
+        success: function(data, textStatus, jqXHR)
+        {
+            console.log('saveStatus: Ajax post was a success!');
+        },
+      }); 
+    };
+
+
+
+
+
+
+
 function PMtraceStart(el){
       console.log('traceStart !!!!!')
       //formatStartUrl = raw_urlPath.split('/');
       //https://gradientone-test.appspot.com/panelcontrol/Acme/Tahoe/Primetime
       var startValue = 'Start_Trace';
-      var start_url = window.location.origin + '/panelcontrol/Acme/Tahoe/Primetime';// + formatStartUrl[formatStartUrl.length-2];
+      var start_url = window.location.origin + '/panelcontrol/Acme/Tahoe/' + configName;// + formatStartUrl[formatStartUrl.length-2];
       console.log('exploreStart: start_url =',start_url);
       var startData = JSON.stringify({"command":startValue});
       console.log('exploreStart: startData =', startData);
@@ -23,7 +80,7 @@ function PMtraceStart(el){
       console.log('traceStop !!!!!')
       
       var startValue = 'Stop_Trace';
-      var start_url = window.location.origin + '/panelcontrol/Acme/Tahoe/Primetime';// + formatStartUrl[formatStartUrl.length-2];
+      var start_url = window.location.origin + '/panelcontrol/Acme/Tahoe/' + configName;// + formatStartUrl[formatStartUrl.length-2];
       console.log('exploreStart: start_url =',start_url);
 
       var startData = JSON.stringify({"command":startValue});
@@ -81,56 +138,5 @@ function powerMeterData() {
 
 
 
-
-
-//PM CONFIG FORM SUBMIT
-    function PMConfig() {
-      $('#collapseConfig').collapse("hide");
-      var d = document.getElementById('PMConfigSettings');
-
-      var children = d.childNodes;
-      console.log('PMConfig: children = ', children);
-      var configName = children[1].children[1].value;
-      var traceName = children[3].children[1].value;
-
-      var frequencyCorrection =  children[5].children[1].value;
-      var offset = children[7].children[1].value; 
-      var units = children[9].children[1].value; 
-      var avgCountAuto = children[11].children[1].checked; 
-      var rangeAuto = children[13].children[1].checked;
-
-      var configSettings = document.getElementById('PMSettingsDisplay');
-      var setChildren = configSettings.childNodes;
-
-
-
-      setChildren[1].children[1].innerHTML = configName;
-      setChildren[3].children[1].innerHTML = frequencyCorrection;
-      setChildren[1].children[3].innerHTML = traceName;
-      setChildren[3].children[3].innerHTML = offset;
-
-      document.getElementById("PMUnits").innerHTML = units;
-    
-
-      var config_url = window.location.origin + '/u2000_configinput';
-      console.log('saveStatus: config_url = ',config_url);
-
-      var configSettings = JSON.stringify({"config_name":configName,"trace_name":traceName, "correction_frequency":frequencyCorrection,
-       "offset":offset, "units":units, "avg_count_auto":avgCountAuto, "range_auto":rangeAuto, "hardware_name":"Tahoe",
-       "inst_name":"U2001A","company_nickname":"Acme"});
-
-      console.log('instConfig: configSettings = ',configSettings);
-
-     $.ajax({
-        type: "POST",
-        url: config_url,
-        data: configSettings,
-        dataType: 'json',
-        success: function(data, textStatus, jqXHR)
-        {
-            console.log('saveStatus: Ajax post was a success!');
-        },
-      }); 
-    };
 
 
