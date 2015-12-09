63cc7ff66d877dcf17c7cdb0d5773a742faefc82
Nick Edwards <nedwards@gradientone.com>
2015-12-09 10:07:22 -0800

    pylint fixes

--- Python/Instruments/new_u2000_post.py
+++ Python/Instruments/new_u2000_post.py
@@ -42,19 +42,26 @@ class agilentu2000:
     GAE_INSTANCE = 'gradientone-test'
     USERNAME = 'nedwards'
 
-    def dt2ms(self, t):
-        return int(t.strftime('%s'))*1000 + int(t.microsecond/1000)
+    def dt2ms(dtime):
+        """Converts date time to miliseconds
+        >>> from new_u2000_client import dt2ms
+        >>> import datetime
+        >>> dtime = datetime.datetime(2015, 12, 8, 18, 11, 44, 320012)
+        >>> dt2ms(dtime)
+        1449627104320
+        """
+        return int(dtime.strftime('%s'))*1000 + int(dtime.microsecond/1000)
 
-    def __init__(self, u2000_test_results, s):
+    def __init__(self, u2000_test_results, ses):
         self.u2000_test_results = u2000_test_results
-        self.s = s
+        self.ses = ses
 
     def post_creation_data(self, i_settings, stuffing, start_tse,
                           config_name, active_testplan_name, test_plan):
         """ post_creation_data function sends a json object that tells
            the browser where to get data for plotting
         """
-        s = self.s
+        ses = self.ses
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         if test_plan == True:
             raw_data_url = ("https://" + GAE_INSTANCE +
@@ -72,10 +79,10 @@ class agilentu2000:
                            'testplan_name':active_testplan_name,
                            'hardware_name':HARDWARENAME})
             out_u2000 = json.dumps(window_u2000, ensure_ascii=True)
-            r = s.post(url_t, data=out_u2000, headers=headers)
+            r = ses.post(url_t, data=out_u2000, headers=headers)
             #print "dir(r)=", dir(r)
-            print "r.reason=", r.reason
-            print "r.status_code=", r.status_code
+            print "result.reason=", result.reason
+            print "result.status_code=", result.status_code
         else:
             raw_data_url = ("https://" + GAE_INSTANCE
                            + ".appspot.com/u2000data/"
@@ -90,10 +97,10 @@ class agilentu2000:
                            'testplan_name':active_testplan_name,
                            'hardware_name':HARDWARENAME})
             out_u2000 = json.dumps(window_u2000, ensure_ascii=True)
-            r = s.post(url_t, data=out_u2000, headers=headers)
+            r = ses.post(url_t, data=out_u2000, headers=headers)
             #print "dir(r)=", dir(r)
-            print "r.reason=", r.reason
-            print "r.status_code=", r.status_code
+            print "result.reason=", result.reason
+            print "result.status_code=", result.status_code
 
     def post_complete(self, active_testplan_name, config_name,
                      test_plan, stop_tse, i_settings,
@@ -101,7 +108,7 @@ class agilentu2000:
         """post_complete function sends a json object that can
            tells the server the test is complete
         """
-        s = self.s
+        ses = self.ses
         window_complete = ({'active_testplan_name':active_testplan_name,
                            'cha':test_results, 'config_name':config_name,
                            'test_plan':test_plan, 'stop_tse':stop_tse,
@@ -112,9 +119,9 @@ class agilentu2000:
         url_c = ("https://" + GAE_INSTANCE +".appspot.com/u2000_testcomplete/"
                 + COMPANYNAME + '/' + active_testplan_name
                 + '/' +config_name + "/%s" % str(stop_tse))
-        c = s.post(url_c, data=out_complete, headers=headers)
-        print "c.reason=", c.reason
-        print "c.status_code=", c.status_code
+        c = ses.post(url_c, data=out_complete, headers=headers)
+        print "result.reason=", result.reason
+        print "result.status_code=", result.status_code
         #print "dir(c)=", dir(c)
 
     def transmitraw(self):
@@ -166,27 +173,27 @@ class agilentu2000:
         if blob_u2k_tr['test_plan'] == 'False':
             blob_u2k_tr['measurement_source'] = 'Instrument'
         else:
-            blob_u2k_tr['measurement_source'] = 'Testplan'    
+            blob_u2k_tr['measurement_source'] = 'Testplan'
         blob_u2k_tr['pass_fail_type'] = blob_u2k_tr['i_settings']['pass_fail_type']
         blob_u2k_tr['hardware_name'] = HARDWARENAME
         del blob_u2k_tr['i_settings']
-        filename = config_name + ':' + active_testplan_name 
-        f = open('/home/' + USERNAME + '/' + COMPANYNAME + '/Blobs/tempfile.csv', 'w')
-        w = csv.writer(f)
+        filename = config_name + ':' + active_testplan_name
+        fileblob = open('/home/' + USERNAME + '/' + COMPANYNAME + '/Blobs/tempfile.csv', 'w')
+        w = csv.writer(fileblob)
         w.writerow(blob_u2k_tr.keys())
         w.writerow(blob_u2k_tr.values())
-        f.close()
+        fileblob.close()
         m = MultipartEncoder(
-                   fields={'field0':(filename, open('/home/' 
-                   + USERNAME + '/' + COMPANYNAME 
+                   fields={'field0':(filename, open('/home/'
+                   + USERNAME + '/' + COMPANYNAME
                    + '/Blobs/tempfile.csv', 'rb'), 'text/plain')}
                    )
         blob_url = requests.get("https://" 
                    + GAE_INSTANCE + ".appspot.com/upload/geturl")
         b = requests.post(blob_url.text, data = m, 
                           headers={'Content-Type': m.content_type})
-        print "b.reason=", b.reason
-        print "b.status_code=", b.status_code
+        print "result.reason=", result.reason
+        print "result.status_code=", result.status_code
 
 if __name__ == "__main__":
     import doctest
