0ab29939b5fcc0bb0083905514cbb8494838e001
Nick Edwards <nedwards@gradientone.com>
2015-12-13 10:18:31 -0800

    fix stupid bug

--- Python/Instruments/new_u2000_run_client.py
+++ Python/Instruments/new_u2000_run_client.py
@@ -141,8 +141,8 @@ def check_config_url():
         elif config['configs_run']:
             nested_config = config['nested_config'][0]
             config = config['configs_run'][0]
-            if config['commence_run']:
-                print "Starting API" == 'True'
+            if config['commence_run'] == 'True':
+                print "Starting API" 
                 post_status('Starting')
                 u2000_acq_run(config, nested_config, ses, headers)
                 # config_vars = check_config_vars(config, nested_config)
