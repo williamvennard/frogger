df7c210a718bcc1668f5123fee3036ab4d2e9933
Nick Edwards <nedwards@gradientone.com>
2015-12-16 00:47:49 -0800

    fixing key

--- Python/Instruments/new_u2000_client.py
+++ Python/Instruments/new_u2000_client.py
@@ -225,8 +225,8 @@ def u2000_acq(config, nested_config, meas, ses):
     # inst_dict = set_v_for_k(inst_dict, 'min_value', config_vars[10])
     inst_dict = set_v_for_k(inst_dict, 'pass_fail', meas['pass_fail'])
     inst_dict = set_v_for_k(inst_dict, 'pass_fail_type', 'Range') #needs more work, range is a placeholder
-    inst_dict = set_v_for_k(inst_dict, 'max_value', meas['max_value'])
-    inst_dict = set_v_for_k(inst_dict, 'min_value', meas['min_value'])
+    inst_dict = set_v_for_k(inst_dict, 'max_value', meas['max_pass'])
+    inst_dict = set_v_for_k(inst_dict, 'min_value', meas['min_pass'])
     inst_dict = set_v_for_k(inst_dict, 'offset', config_vars[3])
     inst_dict = set_v_for_k(inst_dict, 'active_testplan_name', config_vars[0])
     inst_dict = set_v_for_k(inst_dict, 'test_plan', config_vars[6])
